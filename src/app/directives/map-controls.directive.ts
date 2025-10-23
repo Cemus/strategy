9ff34cba2493f20ce568097b80fa70e6823e7c1d
Cemus <24947582+Cemus@users.noreply.github.com>
2025-10-23 18:06:07 +0200

    Fix : Event listeners instead of @HostListeners (Chrome [Violation] : non-passive event)

--- src/app/directives/map-controls.directive.ts
+++ src/app/directives/map-controls.directive.ts
@@ -1,10 +1,4 @@
-import {
-  Directive,
-  ElementRef,
-  HostListener,
-  OnInit,
-  OnDestroy,
-} from '@angular/core';
+import { Directive, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import { GameStoreService } from '../core/services/game-store.service';
 import { Subscription } from 'rxjs';
 
@@ -15,7 +9,6 @@ import { Subscription } from 'rxjs';
 export class MapControlsDirective implements OnInit, OnDestroy {
   private svgRef!: SVGSVGElement | null;
 
-  private isDragging = false;
   private dragStart = { x: 0, y: 0 };
   private translate = { x: 0, y: 0 };
   private scale = 1;
@@ -28,9 +21,7 @@ export class MapControlsDirective implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.svgRef = this.elRef.nativeElement.querySelector('svg')!;
-    this.elRef.nativeElement.addEventListener('wheel', this.onWheel, {
-      passive: false,
-    });
+    this.setupListeners();
     this.zoomSubscription = this.gameStore.zoomScale$.subscribe((scale) => {
       this.scale = scale;
       this.applyTransform();
@@ -38,25 +29,16 @@ export class MapControlsDirective implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.elRef.nativeElement.removeEventListener('wheel', this.onWheel);
+    this.removeListeners();
     this.zoomSubscription?.unsubscribe();
   }
 
-  @HostListener('mousedown', ['$event'])
   onMouseDown(event: MouseEvent) {
-    this.isDragging = true;
     this.dragStart = { x: event.clientX, y: event.clientY };
   }
 
-  @HostListener('mouseup')
-  @HostListener('mouseleave')
-  onMouseUp() {
-    this.isDragging = false;
-  }
-
-  @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
-    if (!this.isDragging) return;
+    if (event.buttons === 0) return;
 
     const dx = event.clientX - this.dragStart.x;
     const dy = event.clientY - this.dragStart.y;
@@ -68,25 +50,16 @@ export class MapControlsDirective implements OnInit, OnDestroy {
     this.applyTransform();
   }
 
-  @HostListener('touchstart', ['$event'])
   onTouchStart(event: TouchEvent) {
     const touch = event.touches[0];
 
     if (!touch) return;
 
-    this.isDragging = true;
     this.dragStart = { x: touch.clientX, y: touch.clientY };
   }
 
-  @HostListener('touchend')
-  @HostListener('touchcancel')
-  onTouchEnd() {
-    this.isDragging = false;
-  }
-
-  @HostListener('touchmove', ['$event'])
   onTouchMove(event: TouchEvent) {
-    if (!this.isDragging || event.touches.length === 0) return;
+    if (event.touches.length === 0) return;
 
     const touch = event.touches[0];
     const dx = touch.clientX - this.dragStart.x;
@@ -97,12 +70,9 @@ export class MapControlsDirective implements OnInit, OnDestroy {
     this.dragStart = { x: touch.clientX, y: touch.clientY };
 
     this.applyTransform();
-    event.preventDefault();
   }
 
   private onWheel = (event: WheelEvent) => {
-    event.preventDefault();
-
     const zoomSpeed = 0.005;
     this.scale += -event.deltaY * zoomSpeed;
     this.scale = Math.max(3, Math.min(7, this.scale));
@@ -114,5 +84,59 @@ export class MapControlsDirective implements OnInit, OnDestroy {
   private applyTransform() {
     if (!this.svgRef) return;
     this.svgRef.style.transform = `translate(${this.translate.x}px, ${this.translate.y}px) scale(${this.scale})`;
+  }
+
+  private setupListeners() {
+    this.elRef.nativeElement.addEventListener('wheel', this.onWheel, {
+      passive: true,
+    });
+    this.elRef.nativeElement.addEventListener(
+      'touchmove',
+      (e) => this.onTouchMove(e),
+      {
+        passive: true,
+      }
+    );
+
+    this.elRef.nativeElement.addEventListener(
+      'touchstart',
+      (e) => this.onTouchStart(e),
+      {
+        passive: true,
+      }
+    );
+
+    this.elRef.nativeElement.addEventListener(
+      'mousedown',
+      (e) => this.onMouseDown(e),
+      {
+        passive: true,
+      }
+    );
+    this.elRef.nativeElement.addEventListener(
+      'mousemove',
+      (e) => this.onMouseMove(e),
+      {
+        passive: true,
+      }
+    );
+  }
+
+  private removeListeners() {
+    this.elRef.nativeElement.removeEventListener('wheel', this.onWheel);
+    this.elRef.nativeElement.removeEventListener('touchmove', (e) =>
+      this.onTouchMove(e)
+    );
+
+    this.elRef.nativeElement.removeEventListener('touchstart', (e) =>
+      this.onTouchStart(e)
+    );
+
+    this.elRef.nativeElement.removeEventListener('mousedown', (e) =>
+      this.onMouseDown(e)
+    );
+    this.elRef.nativeElement.removeEventListener('mousemove', (e) =>
+      this.onMouseMove(e)
+    );
   }
 }
